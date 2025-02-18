8a0993bd6b5ffed8febc2cdaf53f9a033240b361
nicolepaulina027 <nicolepaulina027@gmail.com>
2025-02-18 14:09:14 -0400

    Add files via upload

---
+++ arrays02.js
@@ -0,0 +1,15 @@
+let comida = []
+comida.push('Uva', 'Fresa','Pera','Arandonos')
+console.log(comida[3]);
+comida.unishift('Keiru');
+console.log(comida);
+comida[4] = 'Uva'
+console.log(comida);
+comida.shift(comida);
+console.log(comida);
+comida.splice(2,1)
+console.log(comida)
+delete(comida[1])
+console.log(comida)
+comida[1] = 'Frutas'
+console.log(comidad)
